afaef3abf78cb9edce2d42b6fcfc504a80f9b354
Rich Youngkin <rich.youngkin@gmail.com>
2017-11-04 15:22:39 -0600

    Initial version

---
+++ quiz.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Read file, whole or line-by-line?
+	// Parse file
+	// Write to stdout
+	// Wait for stdin
+	x := 1
+	fmt.Printf("Printing x: %d\n", x)
+}
