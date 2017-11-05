1c77951c14c7cf30c703dff22cd933fc99440e40
Rich Youngkin <rich.youngkin@gmail.com>
2017-11-05 16:33:11 -0700

    Add timeout capability.

--- quiz.go
+++ quiz.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"bufio"
+	"context"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"strings"
 	"time"
@@ -17,33 +19,58 @@ func main() {
 	f, err := os.Open(*fname)
 	if err != nil {
 		fmt.Printf("Error opening file %s: %s\n", *fname, err)
+		os.Exit(-1)
 	}
 	defer f.Close()
 
-	scanner := bufio.NewScanner(f)
-	in := bufio.NewReader(os.Stdin)
-	var total, correct int
+	var bytes []byte
+	if bytes, err = ioutil.ReadAll(f); err != nil {
+		fmt.Printf("Error reading bytes from file %s: Error %s", *fname, err)
+		os.Exit(-2)
+	}
+
+	// Get set of questions delimited by newlines
+	quests := strings.Split(string(bytes), "\n")
+	total := float32(len(quests))
+
+	ctx, canFunc := context.WithTimeout(context.Background(), time.Second*time.Duration(*timeout))
+	defer canFunc()
 
 	timer := time.NewTimer(time.Second * time.Duration(*timeout))
+	c := make(chan string)
+	var correct float32
 
 Loop:
-	for scanner.Scan() {
+	for _, qna := range quests {
+		q := qna[:strings.Index(qna, ",")]
+		a := qna[strings.Index(qna, ",")+1:]
+
+		go getUserInput(ctx, q, c)
+
 		select {
 		case <-timer.C:
-			fmt.Println("Time's up!")
+			fmt.Println("\n\nTime's up!")
 			break Loop
-		default:
-			total++
-			qna := strings.Split(scanner.Text(), ",")
-			fmt.Print(qna[0], " = ")
-			ans, _ := in.ReadString('\n')
+		case ans := <-c:
 			ans = strings.TrimRight(ans, "\n")
-			if ans == qna[1] {
+			if ans == a {
 				correct++
 			}
 		}
 	}
 
-	fmt.Printf("\nTotal number of questions: %d, correct answers: %d, score:%d\n",
-		total, correct, correct/total*100)
+	fmt.Printf("\nTotal number of questions: %.f, correct answers: %.f, score:%2.f percent\n",
+		total, correct, float32((correct/total)*100))
+}
+
+func getUserInput(ctx context.Context, q string, c chan<- string) {
+	fmt.Print(q, " = ")
+	in := bufio.NewReader(os.Stdin)
+	ans, _ := in.ReadString('\n')
+	select {
+	case c <- ans:
+		return
+	case <-ctx.Done():
+		return
+	}
 }
